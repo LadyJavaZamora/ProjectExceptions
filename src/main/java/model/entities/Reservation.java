4cdec2412500164d5c1b17925ed46546d6cf60b8
Marita Zamora <107961731+LadyJavaZamora@users.noreply.github.com>
2022-12-14 22:37:51 -0300

    Melhor solucao tratamento de excecao

--- src/main/java/model/entities/Reservation.java
+++ src/main/java/model/entities/Reservation.java
@@ -4,21 +4,25 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
+
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
 	
-	
-	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
-		this.roomNumber = roomNumber;
-		this.checkIn = checkin;
-		this.checkOut = checkout;
-	}
-
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
-	
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
 	public Integer getRoomNumber() {
 		return roomNumber;
 	}
@@ -36,28 +40,21 @@ public class Reservation {
 	}
 
 	public long duration() {
-	   long diff = checkOut.getTime() - checkIn.getTime();
-	   return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	//Soluçao ruim
-	public String updateDates(Date checkIn, Date checkOut) {
-		
-		//Parte da validaçao na classe - menos ruim
+	public void updateDates(Date checkIn, Date checkOut) {
 		Date now = new Date();
-		 if (checkIn.before(now) || checkOut.before(now)) {
-			return "Reservation dates for update must be future dates";
+		if (checkIn.before(now) || checkOut.before(now)) {
+			throw new DomainException("Reservation dates for update must be future dates");
 		}
 		if (!checkOut.after(checkIn)) {
-			return "Check-out date must be after check-in date";
-		}		
-		//fim inclusão validação
-		 
+			throw new DomainException("Check-out date must be after check-in date");
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;//nao teve erro ao mudar datas
 	}
-	//
 	
 	@Override
 	public String toString() {
@@ -71,5 +68,4 @@ public class Reservation {
 			+ duration()
 			+ " nights";
 	}
-
 }
